95d986e6b9bee401b8a6819ed4d7624922bcb1c2
Yogananda Sai Thanniru <thanniruyogananda@gmail.com>
2023-06-14 00:07:09 -0400

    commit

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBymkNDtUoQVaGOCluXrMfjRzMnAic42fU",
+  authDomain: "chat-application-c2295.firebaseapp.com",
+  projectId: "chat-application-c2295",
+  storageBucket: "chat-application-c2295.appspot.com",
+  messagingSenderId: "207970474664",
+  appId: "1:207970474664:web:5b51d5492a6eb7cf12409d"
+};
+
+
+export const app = initializeApp(firebaseConfig);
